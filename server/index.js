e1c2462291b2a0b7bccf94dc252d4b635380d64b
Kartik Sahu <kartik.sahu3311@gmail.com>
2026-04-22 00:06:06 +0530

    fixed cors

--- server/index.js
+++ server/index.js
@@ -74,10 +74,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"*",
 		credentials:true,
 	})
-)
+);
 
 app.use(
 	fileUpload({
